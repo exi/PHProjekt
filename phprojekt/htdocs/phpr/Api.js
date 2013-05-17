2c59f5615905ebc7bdf9e4a1752e14438f5ca419
Reno Reckling <reno.reckling@mayflower.de>
2013-05-17 10:48:35 +0200

    use projects model in api projectTitleForId

--- phprojekt/htdocs/phpr/Api.js
+++ phprojekt/htdocs/phpr/Api.js
@@ -39,14 +39,11 @@ define([
         var titlesById = null;
         var def = new Deferred();
 
-        exports.getData(
-            'index.php/Project/Project',
-            {query: {projectId: 1, recursive: true}}
-        ).then(function(projects) {
+        projects.getProjects().then(function(projects) {
             titlesById = {};
-            array.forEach(projects, function(p) {
-                titlesById[p.id] = p.title;
-            });
+            for (var id in projects) {
+                titlesById[id] = projects[id].title;
+            }
 
             def.resolve(titlesById);
             def = null;
